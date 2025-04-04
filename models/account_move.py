87b1ddcb4f10b78caeeb9fd1accfd005af28d30b
klap <klap50@gmail.com>
2025-04-04 11:19:41 -0300

    asdasdasdasd

--- models/account_move.py
+++ models/account_move.py
@@ -3,13 +3,11 @@ from odoo import models, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    @api.onchange('invoice_line_ids')
-    def _apply_rg5329_tax(self):
+    def _evaluate_rg5329_tax(self):
         tax = self.env.ref('rg5329_tax_module.tax_rg5329_pais_25', raise_if_not_found=False)
         if not tax:
             return
 
-        # Calcular el total de líneas alcanzadas por RG 5329
         rg_total = sum(
             line.price_subtotal
             for line in self.invoice_line_ids
@@ -17,10 +15,24 @@ class AccountMove(models.Model):
         )
 
         for line in self.invoice_line_ids:
-            if line.product_id.apply_rg5329 and rg_total > 100000:
-                if tax not in line.tax_ids:
-                    line.tax_ids += tax
-            else:
-                if tax in line.tax_ids:
-                    line.tax_ids -= tax
+            if line.product_id.apply_rg5329:
+                if rg_total > 100000:
+                    if tax not in line.tax_ids:
+                        line.tax_ids += tax
+                else:
+                    if tax in line.tax_ids:
+                        line.tax_ids -= tax
 
+    @api.onchange('invoice_line_ids')
+    def _onchange_apply_rg5329_tax(self):
+        self._evaluate_rg5329_tax()
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._evaluate_rg5329_tax()
+        return res
+
+    def create(self, vals):
+        record = super().create(vals)
+        record._evaluate_rg5329_tax()
+        return record

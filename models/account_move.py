37622e43cadb3f9c69ac6a1c3557ebf6bedbbb7e
klap <klap50@gmail.com>
2025-04-04 11:00:44 -0300

    asd

--- models/account_move.py
+++ models/account_move.py
@@ -5,15 +5,22 @@ class AccountMove(models.Model):
 
     @api.onchange('invoice_line_ids')
     def _apply_rg5329_tax(self):
-        threshold = 3000.0
         tax = self.env.ref('rg5329_tax_module.tax_rg5329_pais_25', raise_if_not_found=False)
         if not tax:
             return
-        total_tax = sum(line.price_total - line.price_subtotal for line in self.invoice_line_ids)
+
+        # Calcular el total de líneas alcanzadas por RG 5329
+        rg_total = sum(
+            line.price_subtotal
+            for line in self.invoice_line_ids
+            if line.product_id.apply_rg5329
+        )
+
         for line in self.invoice_line_ids:
-            if total_tax > threshold and line.product_id.apply_rg5329:
+            if line.product_id.apply_rg5329 and rg_total > 100000:
                 if tax not in line.tax_ids:
                     line.tax_ids += tax
             else:
                 if tax in line.tax_ids:
                     line.tax_ids -= tax
+
